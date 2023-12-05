a92965c6ad06ad65aa9427281e22f0e4f7725174
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 09:46:44 -0800

    adding the condtion if both tuples are NULL

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,5 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0,)
     elif len(tuple_b) == 1:
         tuple_b = tuple_b + (0,)
+    elif tuple_a == () and tuple_b == ():
+        return None
     result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return result
