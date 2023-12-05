e59796bb6d80088bfc16eef578d565ec697afd28
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 10:11:04 -0800

    restructuring if else code'

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,9 +8,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         result = (tuple_a[0] + tuple_b[0], 0)
     elif len(tuple_a) == 1:
-        tuple_a = tuple_a + (0,)
+        result = (tuple_a[0] + tuple_b[0], tuple_b[1])
     elif len(tuple_b) == 1:
-        tuple_b = tuple_b + (0,)
+        result = (tuple_a[0] + tuple_b[0], tuple_a[1])
     else:
         result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return result
