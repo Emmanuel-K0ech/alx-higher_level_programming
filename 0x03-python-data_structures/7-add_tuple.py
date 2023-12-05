df9d93eaf76ce4f575e64627eea639471aadbecd
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 10:00:39 -0800

    adding elif statment for both tuples being len == 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,13 +5,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_b
     elif len(tuple_b) == 0:
         return tuple_a
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        result = (tuple_a[0] + tuple_b[0], 0)
     elif len(tuple_a) == 1:
         tuple_a = tuple_a + (0,)
-        add_tuple(tuple_a=(), tuple_b=())
     elif len(tuple_b) == 1:
         tuple_b = tuple_b + (0,)
-        add_tuple(tuple_a=(), tuple_b=())
     elif tuple_a == () and tuple_b == ():
         return None
-    result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    else:
+        result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return result
