afda2d3a4c38103b0af14a41faf25ad963e111e2
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 09:53:19 -0800

    adding recursive fuction to avoid missing elif statements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,8 +7,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_a
     elif len(tuple_a) == 1:
         tuple_a = tuple_a + (0,)
+        add_tuple(tuple_a=(), tuple_b=())
     elif len(tuple_b) == 1:
         tuple_b = tuple_b + (0,)
+        add_tuple(tuple_a=(), tuple_b=())
     elif tuple_a == () and tuple_b == ():
         return None
     result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
