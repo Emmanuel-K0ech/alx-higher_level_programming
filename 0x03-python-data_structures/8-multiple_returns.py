e244e31a3e5086272dafabd4de198e7aa676990f
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 23:08:15 -0800

    task 8: multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    letter = None
+    length = 0
+    if sentence is None:
+        return length, letter
+    else:
+        length = len(sentence)
+        letter = sentence[0]
+        return length, letter
