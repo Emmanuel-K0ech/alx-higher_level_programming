3684516517002d8d22d5ac7bf312dfdb073a30c3
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-05 22:47:44 -0800

    if both tuples are empty return (0, 0)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    result = ()
+    result = (0, 0)
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return result
     if len(tuple_a) == 0:
         return tuple_b
     elif len(tuple_b) == 0:
