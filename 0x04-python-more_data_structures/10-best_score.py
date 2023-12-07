a85b11695ef5cba0d7bc40aabd74490c01a4325f
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-07 04:01:35 -0800

    correction for scenarion of empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     name = ""
     score = 0
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     for k, v in a_dictionary.items():
         if v > score:
