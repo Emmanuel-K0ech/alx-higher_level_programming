5bec4f00263b5698fb1d0b2de53ae66836714112
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-10 03:48:20 -0800

    adding documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Function read file
+    filename:
+        file to be read
+    Returns:
+        void
+    """
     with open(filename) as f:
-       print(f.read(), end="")
+        print(f.read(), end="")
