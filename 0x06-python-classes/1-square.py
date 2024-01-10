bdab50a5fcb448376bad831538f3bf7c27dbdf2b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-10 14:26:48 -0800

    task 1: square class implementation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """ Defines a square """
+    def __init__(self, size):
+        """ initialization method
+
+            Args:
+                _size: dimension of the square
+        """
+        self._size = size
