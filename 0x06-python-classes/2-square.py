1746e7fa1f7d501aff3706674b69df18787a1ee0
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-10 14:57:21 -0800

    task 2: data validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This is a module to a class(square) """
+
+
+class Square:
+    """ This is a class with a private field and __init__ method """
+    def __init__(self, size=0):
+        """ initializes the following fields
+
+            Args:
+                __size: dimensions of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
