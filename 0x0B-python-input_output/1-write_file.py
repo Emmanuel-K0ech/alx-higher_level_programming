3e68d6d6f86baa45deee82ab505cb078f22bfcf1
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-16 05:49:19 -0800

    documenting module and function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+""" The function writes a string to a text file """
+
+
 def write_file(filename="", text=""):
+    """
+    Writes to a file and returns num of char written
+
+        Arg:
+            filename: file to write
+            text: text to  write on file
+        Return:
+            number of char written
+    """
     with open(filename, 'w') as f:
         written_char = f.write(text)
         return written_char
