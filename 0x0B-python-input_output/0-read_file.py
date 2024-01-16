01db571388cacd58042712b3e055e4e11a546823
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-16 05:44:39 -0800

    documenting function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""
-    Function that reads a text file and prints to stdout
-"""
+"""Function that reads a text file and prints to stdout"""
 
 
 def read_file(filename=""):
+    """
+    This is a read file function
+
+    Arg:
+        filename: name of file to read
+    """
     with open(filename) as f:
         print(f.read(), end="")
