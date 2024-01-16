73e4df40b76ab578debd05be7c38ed762d578649
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-16 05:53:32 -0800

    documentation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+""" Function to append to a file """
+
+
 def append_write(filename="", text=""):
+    """
+    Appends to a file and returns the number of characters added
+
+        Arg:
+            filename: file to append to
+            text: chars to append
+
+        Return: number of characters appended
+    """
     with open(filename, 'a', encoding="utf-8") as f:
         written_text = f.write(text)
         return written_text
