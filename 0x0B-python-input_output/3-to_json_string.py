a0cd56abb53fa62105542adfc5c3c3d3d4ea6394
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-17 04:28:01 -0800

    Documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""The function below converts object to json string"""
 import json
 
 
 def to_json_string(my_obj):
+    """
+    Arg:
+        my_obj: object to convert to JSON
+
+    Return: JSON representation of obj(str)
+    """
     json_str = json.dumps(my_obj)
     return json_str
