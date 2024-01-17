8b7a0de13a367e6ca44547c9a9ccc8be03cc8858
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-01-17 04:34:37 -0800

    Documentation: Module and class

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+    Function returns an object
+    from JSON representation string
+"""
 import json
 
 
 def from_json_string(my_str):
+    """
+    Arg:
+        my_str: JSON string to convert to object
+
+    Return: object for JSON string
+    """
     my_obj = json.loads(my_str)
     return my_obj
