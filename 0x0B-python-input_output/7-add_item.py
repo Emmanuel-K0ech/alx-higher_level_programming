350c982669f8037bf0fa011652d1f541dfeb9715
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-02-01 08:16:10 -0800

    mode of writing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,8 +13,9 @@ module_name1 = "5-save_to_json_file"
 module_name2 = "6-load_from_json_file"
 save_module = importlib.import_module(module_name1)
 load_module = importlib.import_module(module_name2)
-add_list = sys.argv[1:]
+new_args = []
+new_args = sys.argv[1:]
 json_filename = "add_item.json"
 with open(json_filename, 'a') as f:
-    save_module.save_to_json_file(add_list, json_filename)
+    save_module.save_to_json_file(new_args, json_filename)
     my_obj = load_module.load_from_json_file(json_filename)
