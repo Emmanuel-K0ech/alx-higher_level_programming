8451b697bebcb8d576c22df67ee56ad322992305
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-02-14 07:27:31 -0800

    Show only fields name and score

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- List all record of the table second_table
 -- Dont list rows without a name value
-SELECT * FROM second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
