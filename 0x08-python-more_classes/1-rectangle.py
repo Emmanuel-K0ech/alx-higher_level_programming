02539d484da2d2d831a2231433f3b0ac953ed8e1
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-21 13:16:44 -0700

    changing order of variables

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
         height(int): demension of rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
