d663a11ec71ac7bc4393df8d9ed398c013f15ce7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-21 13:59:24 -0700

    Task 3: __str__()

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Module 3-rectangle.py """
+
+
+class Rectangle:
+    """ Class definition
+    Args
+        width(int): dimension of rectangle
+        height(int): demension of rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculates and returns area of rectacgle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculates and returns perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """ string representation of an object """
+        for x in range(self.__height):
+            print(self.__width * "#")         
