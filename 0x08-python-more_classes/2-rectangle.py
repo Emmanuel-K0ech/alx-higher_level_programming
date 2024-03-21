919456af81a0a50d4680848e548e65aa5bd98d5b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-21 13:35:12 -0700

    Adding self to method calculation

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,7 +48,7 @@ class Rectangle:
 
     def perimeter(self):
         """ Calculates and returns perimeter """
-        if width, height is 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return ((self.__width * 2) + (self.__height * 2))
