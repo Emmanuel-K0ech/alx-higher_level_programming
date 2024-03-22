01a2deb4be17ec389b03b0797a2b7818f3aaf564
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-21 23:35:35 -0700

    Task 4: str() - if 0 return empty string

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -56,6 +56,8 @@ class Rectangle:
     def __str__(self):
         """ string representation of an object """
         representation = ""
+        if self.__width == 0 or self.__height == 0:
+            return representation
         for x in range(self.__height):
             representation += "#" * self.__width + "\n"
         return representation.strip()
