15efb4ec17700105045e02204d86adddae797300
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-08 18:43:51 +0300

    Task 6: Loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+"use strict";
+
+let myArgs = ["Cis fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
+
+while (i < myArgs.length) {
+    console.log(myArgs[i]);
+    i++;
+}
