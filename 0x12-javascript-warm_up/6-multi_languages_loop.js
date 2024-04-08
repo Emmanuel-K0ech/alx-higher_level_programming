67baffd0c671e3f6da08471fb4b5b2415cdf44d7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-08 18:45:27 +0300

    Task 6: type error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 "use strict";
 
-let myArgs = ["Cis fun", "Python is cool", "JavaScript is amazing"];
+let myArgs = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let i = 0;
 
 while (i < myArgs.length) {
