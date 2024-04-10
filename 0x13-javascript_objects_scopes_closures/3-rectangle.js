f632f8d47b8def4de81b07bd87ec0b244c108d76
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-10 19:21:29 +0300

    Task 3: print() method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * print() method, prints dimensions of rectangle using "X"
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
