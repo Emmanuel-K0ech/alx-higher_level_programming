ee51320c4a1d01519ab530ae4acd2e2e82ca669b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-10 20:41:21 +0300

    Task 7: Occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * function that returns the number of occurrences in a list
+ */
+exports.nbOccurences = function (list, searchElement) {
+  let i;
+  let count = 0;
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
