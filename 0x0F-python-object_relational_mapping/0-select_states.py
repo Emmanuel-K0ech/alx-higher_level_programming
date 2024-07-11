fc7410c8bf1be664e384a1a1a40d91a627e58a20
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-11 07:53:00 -0700

    Task 0 : SQLAlchemy

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 "Lists all states from the database hbtn_0e_0_usa:"
+import MySQLdb
+import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                            passwd=argv[2], db=argv[3], charset="utf8")
+
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cr.fetchall()
+    for row in rows:
+        print(row)
+    cr.close()
+    db.close()
