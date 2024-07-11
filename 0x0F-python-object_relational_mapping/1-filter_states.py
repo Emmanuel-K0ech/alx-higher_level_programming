7e3be0defaa2b54dd115657ab9b93f326a4bfdd1
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-11 08:29:51 -0700

    Task 1: making file executable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,14 +2,14 @@
 """ Lists all states with name starting witn 'N' """
 
 import MySQLdb
-form sys import argv
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cr = db.cursor()
     cr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
+    rows = cr.fetchall()
     for row in rows:
         if row[1][0] == 'N':
             print(row)
