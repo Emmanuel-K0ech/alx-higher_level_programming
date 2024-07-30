dea2dce2dbeb0c4ae9e9a47c3de5d866f768acfd
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-31 02:29:48 +0300

    Task 0: semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const readString = fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     return console.error(err);
   }
+  console.log(data);
 });
-console.log(readString);
