837d3c4352756c0f15a0c8439f0bd3a2d011b9c4
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-31 02:49:32 +0300

    Task 2: Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Displays the status code of a GET request : code: <status code>
+const myRequest = new Request(process.argv[2]);
+
+fetch(myRequest)
+  .then((response) => {
+    console.log('code: ', response.status);
+  })
+  .catch((error) => {
+    console.error('Error', error);
+  });
