43eb290998160edce317880b16d06a91449ed593
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-31 03:59:54 +0300

    Task 5: Loripsum - brackets

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,4 +4,4 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
-request(url.pipe(fs.createWriteStream(filePath)));
+request(url).pipe(fs.createWriteStream(filePath));
