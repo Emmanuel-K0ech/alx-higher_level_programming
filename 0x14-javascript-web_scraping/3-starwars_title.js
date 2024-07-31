c2e0347929297db25b6011cc926d02b695fc0117
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-31 03:26:43 +0300

    Task 3: Update API_URL

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // Prints title of a Star Wars movie where episode
 // number matches a given integer
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api/films/:id';
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
 const numEpisode = process.argv[2];
 
 request(API_URL + numEpisode, (err, response, body) => {
